5a0fad9909ac68e370f9bf66c8b94eb410cf3cb7
ben <ben@benfd.uk>
2020-08-28 18:25:00 +0100

    Delete API_KEYS.js

--- src/API_KEYS.js
+++
@@ -1,6 +0,0 @@
-// API KEYS for the app
-
-export const API_KEYS = {
-  guardian_api: "ccdae502-d9b1-4b11-a293-f03e13d47805", // Guardian API Key
-  news_api: "99aeaa3fab684d97a5eaca28f5229095" // Newsapi API key
-};
